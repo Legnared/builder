b392273e0d5f89ef6bed2879e280f4e8847f42d1
Angel <angelguillermohernandezz@gmail.com>
2024-12-27 17:53:02 -0600

    bd

--- web/bd/builder.sql
+++ web/bd/builder.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 24-12-2024 a las 20:23:51
+-- Tiempo de generación: 28-12-2024 a las 00:52:28
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -42,7 +42,30 @@ CREATE TABLE `admins` (
 --
 
 INSERT INTO `admins` (`id_admin`, `email_admin`, `password_admin`, `token_admin`, `token_exp_admin`, `date_created_admin`, `date_updated_admin`) VALUES
-(1, 'admin@builder.com', '$2a$07$azybxcags23425sdg23sdeanQZqjaf6Birm2NvcYTNtJw24CsO5uq', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MzUwNjYzMjcsImV4cCI6MTczNTE1MjcyNywiZGF0YSI6eyJpZCI6IjEiLCJlbWFpbCI6ImFkbWluQGJ1aWxkZXIuY29tIn19.n1iyDCrOpn6rJkMhyhykfbKwgjoWR_HvPAFrc2N9Lt8', '1735152727', '2024-12-20', '2024-12-24 18:52:07');
+(1, 'admin@builder.com', '$2a$07$azybxcags23425sdg23sdeanQZqjaf6Birm2NvcYTNtJw24CsO5uq', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MzUzMTM4NjMsImV4cCI6MTczNTQwMDI2MywiZGF0YSI6eyJpZCI6IjEiLCJlbWFpbCI6ImFkbWluQGJ1aWxkZXIuY29tIn19.YgW5-FUrm-ltNUVAD_WXSJ2IyHxKmSZU4v1l5dD_phM', '1735400263', '2024-12-20', '2024-12-27 15:37:43');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `codes`
+--
+
+CREATE TABLE `codes` (
+  `id_code` int(11) NOT NULL,
+  `id_landing_code` int(11) NOT NULL DEFAULT 0,
+  `html_code` longtext DEFAULT NULL,
+  `css_code` longtext DEFAULT NULL,
+  `javascript_code` longtext DEFAULT NULL,
+  `date_created_code` date DEFAULT NULL,
+  `date_updated_code` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `codes`
+--
+
+INSERT INTO `codes` (`id_code`, `id_landing_code`, `html_code`, `css_code`, `javascript_code`, `date_created_code`, `date_updated_code`) VALUES
+(1, 1, NULL, NULL, NULL, '2024-12-27', '2024-12-27 22:21:19');
 
 -- --------------------------------------------------------
 
@@ -67,7 +90,7 @@ CREATE TABLE `landings` (
 --
 
 INSERT INTO `landings` (`id_landing`, `title_landing`, `url_landing`, `plugins_landing`, `icon_landing`, `cover_landing`, `descripcion_landing`, `date_created_landing`, `date_updated_landing`) VALUES
-(1, 'Agencia de Viajes', 'agencia-de-viajes', '[\"%3Clink+rel%3D%22preconnect%22+href%3D%22https%3A%2F%2Ffonts.googleapis.com%22%3E\",\"%3Clink+rel%3D%22preconnect%22+href%3D%22https%3A%2F%2Ffonts.gstatic.com%22+crossorigin%3E\",\"%3Clink+href%3D%22https%3A%2F%2Ffonts.googleapis.com%2Fcss2%3Ffamily%3DAnton%26family%3DRoboto%3Aital%2Cwght%400%2C100%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C700%3B0%2C900%3B1%2C100%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C700%3B1%2C900%26display%3Dswap%22+rel%3D%22stylesheet%22%3E\"]', 'https://cdn-icons-png.flaticon.com/128/11649/11649745.png', 'https://hd.wallpaperswide.com/thumbs/travel_destinations_3-t2.jpg', 'Lorem Ipsum', '2024-12-24', '2024-12-24 19:22:52');
+(1, 'Agencia de Viajes', 'agencia-de-viajes', '[\"%3Clink+rel%3D%22preconnect%22+href%3D%22https%3A%2F%2Ffonts.googleapis.com%22%3E\",\"%3Clink+rel%3D%22preconnect%22+href%3D%22https%3A%2F%2Ffonts.gstatic.com%22+crossorigin%3E\",\"%3Clink+href%3D%22https%3A%2F%2Ffonts.googleapis.com%2Fcss2%3Ffamily%3DAnton%26family%3DRoboto%3Aital%2Cwght%400%2C100%3B0%2C300%3B0%2C400%3B0%2C500%3B0%2C700%3B0%2C900%3B1%2C100%3B1%2C300%3B1%2C400%3B1%2C500%3B1%2C700%3B1%2C900%26display%3Dswap%22+rel%3D%22stylesheet%22%3E\"]', 'https://cdn-icons-png.flaticon.com/128/11649/11649745.png', 'https://hd.wallpaperswide.com/thumbs/travel_destinations_3-t2.jpg', 'Lorem Ipsum', '2024-12-27', '2024-12-27 22:21:19');
 
 --
 -- Índices para tablas volcadas
@@ -78,6 +101,12 @@ INSERT INTO `landings` (`id_landing`, `title_landing`, `url_landing`, `plugins_l
 --
 ALTER TABLE `admins`
   ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indices de la tabla `codes`
+--
+ALTER TABLE `codes`
+  ADD PRIMARY KEY (`id_code`);
 
 --
 -- Indices de la tabla `landings`
@@ -94,6 +123,12 @@ ALTER TABLE `landings`
 --
 ALTER TABLE `admins`
   MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `codes`
+--
+ALTER TABLE `codes`
+  MODIFY `id_code` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `landings`
